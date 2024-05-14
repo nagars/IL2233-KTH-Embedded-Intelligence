c4218234ff732547afa60fc4e7aee4ede6f35af1
nagars <shawnnagar@gmail.com>
2024-05-14 19:11:34 +0530

    k-means working with current test data. testing with another

--- Lab3/k-means/main.c
+++ Lab3/k-means/main.c
@@ -36,9 +36,9 @@
 */
 double test_data[20] = {1, 1, 2, 2, 3, 3, 4, 4, 4, 4, 7, 7, 7, 7, 8, 8, 9, 9, 10, 10};
 
-int euclidean_distance(double* x, double* y, int m_features){
+double euclidean_distance(double* x, double* y, int m_features){
 
-	int sum = 0;
+	double sum = 0;
 	for(int i = 0; i < m_features; i++){
 		sum += pow((x[i] - y[i]),2);
 	}
@@ -46,7 +46,8 @@ int euclidean_distance(double* x, double* y, int m_features){
 }
 
 int random_number(int min, int max) {
-    srand(time(NULL));
+	//time_t t;
+	//srand((unsigned)time(&t));
     return (rand() % (max - min + 1)) + min;
 }
 
@@ -55,12 +56,12 @@ void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features,
 
 	double* centroids = (double*)malloc(K * m_features * sizeof(double));// Actual centroids
 	//double centroids[K][m_features]
-	double* centroid_datapoints = (double*)malloc(K * sizeof(double));// Tracks the number of data points assigned to each centroid
+	int* centroid_datapoints = (int*)malloc(K * sizeof(int));// Tracks the number of data points assigned to each centroid
 	double* centroid_sums = (double*)malloc(K * m_features * sizeof(double)); // Tracks sum of all datapoints in centroid used to calculate mean
 	//double centroid_sum[K][m_features]
 
 	memset(centroids, 0, K * m_features * sizeof(double));
-	memset(centroid_datapoints, 0, K * sizeof(double));
+	memset(centroid_datapoints, 0, K * sizeof(int));
 	memset(centroid_sums, 0, K * m_features * sizeof(double));
 
 	// Set assignments to 0
@@ -69,6 +70,7 @@ void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features,
 	// Initalise centroids with random data points
 	for(int i = 0; i < K; i++){
 		int random_index = random_number(0, n_samples - 1);
+		//int random_index = i*4;
 		//memcpy(centroids + i*m_features,  data + random_index * m_features, m_features);
 		centroids[i*m_features] = data[random_index * m_features];
 		centroids[i*m_features + 1] = data[random_index * m_features + 1];
@@ -79,11 +81,11 @@ void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features,
 	for(int n = 0; n < max_iter; n++){
 		// iterate through all samples
 		for(int sample = 0; sample < n_samples; sample++){
-			double min_distance = euclidean_distance(data, centroids, m_features);
+			double min_distance = euclidean_distance(&data[sample*m_features], centroids, m_features);
 			//iterate through all centroids
 			for(int centroid_num = 0; centroid_num < K; centroid_num++){
 				// Calculate minimum euclidean distance of all data points to all centroids
-				double distance = euclidean_distance(data + sample*m_features, centroids + centroid_num * m_features, m_features);
+				double distance = euclidean_distance(&data[sample*m_features], &centroids[centroid_num * m_features], m_features);
 				// assign sample to centroid with least distance
 				if(distance < min_distance){
 					min_distance = distance;
@@ -95,8 +97,8 @@ void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features,
 		// iterate through all samples
 		for(int sample = 0; sample < n_samples; sample++){
 			// Calculate sum of features for all datapoints in a specific centroid
-			for(int i = 0; i < m_features; i++){
-				centroid_sums[assignment[sample]*m_features + i] += data[sample*m_features + i];
+			for(int d = 0; d < m_features; d++){
+				centroid_sums[assignment[sample]*m_features + d] += data[sample*m_features + d];
 			}
 			// increment array tracking how many data points in each centroid
 			centroid_datapoints[assignment[sample]]++;
@@ -105,11 +107,13 @@ void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features,
 
 		// Update centroids by calculating mean of assigned data points
 		// iterate through all centroids
-		for(int n = 0; n < K; n++){
+		for(int s = 0; s < K; s++){
 			for(int i = 0; i < m_features; i++){
-				centroids[n * m_features + i] = centroid_sums[n * m_features + i]/centroid_datapoints[n];
+				centroids[s * m_features + i] = centroid_sums[s * m_features + i]/centroid_datapoints[s];
 			}
 		}
+
+		//Check if centroids are moving
 
 		// Clear centroids array
 		memset(centroid_sums, 0, sizeof(double)*K*m_features);
