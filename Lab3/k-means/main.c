ffbbf5b52118c2a486e6355e9288e2095d4ab9c9
nagars <shawnnagar@gmail.com>
2024-05-14 19:20:39 +0530

    Tested with additional test data. working

--- Lab3/k-means/main.c
+++ Lab3/k-means/main.c
@@ -12,29 +12,28 @@
 #include <math.h>
 #include <time.h>
 
-#define threshold 0.1	// Threshold below which centroids can be considered equal
 #define N_SAMPLES	10	// Rows
-#define M_FEATURES	2//3	// Columns
-#define NUM_CENTROIDS	2	// Number of centroids
-#define MAX_ITER	100	// Maximum iterations
+#define M_FEATURES	3	// Columns
+#define NUM_CENTROIDS	3	// Number of centroids
+#define MAX_ITER	1000	// Maximum iterations
 
 
 // For testing algorithm only
-/*const double test_data[] =		//data is formatted as feature1, 2, 3
-{8.3,9.5,1.1,
-		1.2,11.9,10.1,
-		8.3,1.6,10.1,
+const double test_data[] =		//data is formatted as feature1, 2, 3
+{12.3,14.5,1.1,
+		1.2,13.9,12.1,
+		14.3,0.6,13.1,
 		9.5,10.9,0.1,
 		2.3,9.9,11,
-		10.1,0.8,11.2,
-		9.6,9.8,1,
-		2.1,8.9,11,
-		9.6,2.1,10.7,
-		10.9,9.2,0.1};
+		10.1,0.8,10.2,
+		13.6,14.8,1,
+		2.1,10.9,11,
+		14.6,2.1,15.7,
+		13.9,14.2,0.1};
 // End result should be 3 clusters (High feature 1,2 and low feature 3), (Low feature 1 and high 2,3),
 //(low feature 2, high 1,3)
-*/
-double test_data[20] = {1, 1, 2, 2, 3, 3, 4, 4, 4, 4, 7, 7, 7, 7, 8, 8, 9, 9, 10, 10};
+
+//double test_data[20] = {1, 1, 2, 2, 3, 3, 4, 4, 4, 4, 7, 7, 7, 7, 8, 8, 9, 9, 10, 10};
 
 double euclidean_distance(double* x, double* y, int m_features){
 
@@ -68,12 +67,12 @@ void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features,
 	memset(assignment, 0, sizeof(int) * n_samples);
 
 	// Initalise centroids with random data points
-	for(int i = 0; i < K; i++){
+	for(int centroid_num = 0; centroid_num < K; centroid_num++){
 		int random_index = random_number(0, n_samples - 1);
 		//int random_index = i*4;
 		//memcpy(centroids + i*m_features,  data + random_index * m_features, m_features);
-		centroids[i*m_features] = data[random_index * m_features];
-		centroids[i*m_features + 1] = data[random_index * m_features + 1];
+		centroids[centroid_num*m_features] = data[random_index * m_features];
+		centroids[centroid_num*m_features + 1] = data[random_index * m_features + 1];
 	}
 
 
@@ -97,8 +96,8 @@ void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features,
 		// iterate through all samples
 		for(int sample = 0; sample < n_samples; sample++){
 			// Calculate sum of features for all datapoints in a specific centroid
-			for(int d = 0; d < m_features; d++){
-				centroid_sums[assignment[sample]*m_features + d] += data[sample*m_features + d];
+			for(int feature_num = 0; feature_num < m_features; feature_num++){
+				centroid_sums[assignment[sample]*m_features + feature_num] += data[sample*m_features + feature_num];
 			}
 			// increment array tracking how many data points in each centroid
 			centroid_datapoints[assignment[sample]]++;
@@ -107,13 +106,12 @@ void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features,
 
 		// Update centroids by calculating mean of assigned data points
 		// iterate through all centroids
-		for(int s = 0; s < K; s++){
-			for(int i = 0; i < m_features; i++){
-				centroids[s * m_features + i] = centroid_sums[s * m_features + i]/centroid_datapoints[s];
+		for(int centroid_num = 0; centroid_num < K; centroid_num++){
+			for(int feature_num = 0; feature_num < m_features; feature_num++){
+				centroids[centroid_num * m_features + feature_num] = centroid_sums[centroid_num * m_features + feature_num]
+																				   /centroid_datapoints[centroid_num];
 			}
 		}
-
-		//Check if centroids are moving
 
 		// Clear centroids array
 		memset(centroid_sums, 0, sizeof(double)*K*m_features);
